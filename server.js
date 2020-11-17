8b4935fcbb44757946112d1ec28fd0db810e09a9
Angel <diaz.angeledu@gmail.com>
2020-11-17 13:58:54 -0400

    cors initialization

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static("website"));
